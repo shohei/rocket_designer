124e3e4c150e8195fd82f97598931af0b5afaf72
Shohei Aoki <shoaok@gmail.com>
2023-12-19 21:35:00 +0300

    update build script

--- build_app.sh
+++ build_app.sh
@@ -3,6 +3,6 @@
 # 1) Use of pyinstaller: does not work
 #pyinstaller --windowed main.py
 
-# 2) Use of py2app
-py2applet --make-setup main.py
-python setup.py py2app
+# 2) Use of py2app: does not work
+#py2applet --make-setup main.py
+#python setup.py py2app
