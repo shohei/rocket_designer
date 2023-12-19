bbcc11051bbf8d1be2be4f098599e0da17ca5dea
Shohei Aoki <shoaok@gmail.com>
2023-12-19 12:49:57 +0300

    update

--- main.py
+++ main.py
@@ -8,14 +8,31 @@ g = 9.8 #m/s^2
 
 def calculate(ui):
     print("calculate")
-    h = float(ui.le_apogee.text())
+    h = ui.le_apogee.text()
+    if h=='':
+        h = 2.0
+        ui.le_apogee.setText(str(h))
+    else:
+        h = float(h)
+    m0 = ui.le_m0.text()
+    if m0=='':
+        m0 = 10.0
+        ui.le_m0.setText(str(m0))
+    else:
+        m0 = float(m0)
+    Isp = ui.le_isp.text()
+    if Isp=='':
+        Isp = 110
+        ui.le_isp.setText(str(Isp))
+    else:
+        Isp = float(Isp)
+
     deltaV = math.sqrt(2*g*h)
-    
     I = m0*deltaV 
-    MR = 
-    ui.le_deltav.setText(deltaV)
-    ui.le_impulse.setText(I)
-    ui.le_mr.setText(MR)
+    MR = math.exp(deltaV/g*Isp)
+    ui.le_deltav.setText(str(deltaV).format("%.2g"))
+    ui.le_impulse.setText(str(I).format("%.2g"))
+    ui.le_mr.setText(str(MR).format("%.2g"))
 
 def setupHandlers(ui):
     ui.pushButton.clicked.connect(lambda: calculate(ui))
