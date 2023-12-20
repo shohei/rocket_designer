d17516eb7f2a7f5dba2a8dc1bd3ed9750175a7c5
Shohei Aoki <shoaok@gmail.com>
2023-12-20 11:18:23 +0300

    update handler

--- components/thermochemical.py
+++ components/thermochemical.py
@@ -46,6 +46,10 @@ def initializeVariables(ui):
 def run(ui):
     computeCstar(ui)
 
+def setupHandlers(ui):
+    ui.runThermalButton.clicked.connect(lambda: run(ui))
+
 def initialize(ui):
     initializeVariables(ui)
+    setupHandlers(ui)
     computeCstar(ui)
