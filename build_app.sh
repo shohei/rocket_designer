69b2767e7080804fd9f7124c39162895e879414d
Shohei Aoki <shoaok@gmail.com>
2023-12-22 07:56:19 +0300

    update build script

--- build_app.sh
+++ build_app.sh
@@ -3,4 +3,11 @@ pyinstaller -y --windowed src/RocketDesigner.py --icon=src/image/logo_200px.png 
   --add-data="src/image/logo_100px.png:image" --add-data="src/image/Lstar.png:image" \
   --add-data="src/image/pid_ox.png:image" --add-data="src/image/pid_fuel.png:image"
 
-cp /opt/homebrew/lib/python3.9/site-packages/rocketcea/_version.py dist/RocketDesigner.app/Contents/Resources/rocketcea/
+function get_site_package_path(){
+  python -c 'import site; print(site.getsitepackages()[0])'
+}
+SITE_PACKAGE_PATH=$(get_site_package_path)
+echo cp "${SITE_PACKAGE_PATH}/rocketcea/_version.py" dist/RocketDesigner.app/Contents/Resources/rocketcea/
+cp "${SITE_PACKAGE_PATH}/rocketcea/_version.py" dist/RocketDesigner.app/Contents/Resources/rocketcea/
+
+open dist/RocketDesigner.app
