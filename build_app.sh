fb71bf9c050867013f527f1c4cebc45a2377e313
Shohei Aoki <shoaok@gmail.com>
2024-01-05 09:49:33 +0300

    update

--- build_app.sh
+++ build_app.sh
@@ -1,7 +1,11 @@
 #/bin/sh
 pyinstaller -y --windowed src/RocketDesigner.py --icon=src/image/logo_200px.png \
   --add-data="src/image/logo_100px.png:image" --add-data="src/image/Lstar.png:image" \
-  --add-data="src/image/pid_ox.png:image" --add-data="src/image/pid_fuel.png:image"
+  --add-data="src/image/pid_ox.png:image" --add-data="src/image/pid_fuel.png:image" \
+  --add-data="src/doc/mission.html:doc" --add-data="src/doc/thermochemical.html:doc" \
+  --add-data="src/doc/nozzle.html:doc" --add-data="src/doc/chamber.html:doc" \
+  --add-data="src/doc/feed.html:doc" --add-data="src/doc/ignitor.html:doc" \
+  --add-data="src/doc/injector.html:doc" --add-data="src/doc/grain.html:doc" 
 
 function get_site_package_path(){
   python -c 'import site; print(site.getsitepackages()[0])'
